6f2808d05e2a167e17e84eec0c2e716131a02a38
AJ <ajbadarni@gmail.com>
2021-03-09 00:54:25 -0500

    [AJut.Application.WPF] Added better selection change event storage. Use with EventArgs<> - so EventArgs<SelectionChange<int>> for example.

---
+++ Application/AJut.Application.WPF/SelectionChange.cs
@@ -0,0 +1,32 @@
+﻿namespace AJut.Application
+{
+    public class SelectionChange<TElement>
+    {
+        public SelectionChange (bool isClear)
+        {
+            this.IsClear = isClear;
+        }
+
+        public SelectionChange (TElement[] added, TElement [] removed, bool isClear = false)
+        {
+            this.Added = added ?? new TElement[0];
+            this.Removed = removed ?? new TElement[0];
+            this.IsClear = isClear;
+        }
+
+        /// <summary>
+        /// The added items - guranteed to be set unless it's a clear, in which case it will be null.
+        /// </summary>
+        public TElement[] Added { get; }
+
+        /// <summary>
+        /// The removed items - guranteed to be set unless it's a clear, in which case it will be null.
+        /// </summary>
+        public TElement[] Removed { get; }
+
+        /// <summary>
+        /// Was the selection cleared
+        /// </summary>
+        public bool IsClear { get; }
+    }
+}
