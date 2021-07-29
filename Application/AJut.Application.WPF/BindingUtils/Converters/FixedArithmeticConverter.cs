822d2464412581eb07f6b2cd0c0df2103ccec0cd
AJ <ajbadarni@gmail.com>
2021-07-29 08:36:07 -0700

    Added BindingUtils\ folder and move Converters\ into it This to help group utilities like Binding subclasses and others which I've made for such a purpose

---
+++ Application/AJut.Application.WPF/BindingUtils/Converters/FixedArithmeticConverter.cs
@@ -0,0 +1,37 @@
+﻿namespace AJut.Application.Converters
+{
+    using System;
+    using System.Windows;
+
+    public class FixedArithmeticConverter : SimpleValueConverter
+    {
+        public eArithmeticOperation Operation { get; set; } = eArithmeticOperation.Multiply;
+        public double FixedOperand { get; set; }
+
+        protected override object Convert (object value, Type targetType, object parameter)
+        {
+            if (value == DependencyProperty.UnsetValue)
+            {
+                return value;
+            }
+
+            double result = System.Convert.ToDouble(value);
+            
+            switch (this.Operation)
+            {
+                case eArithmeticOperation.Add: result += this.FixedOperand; break;
+                case eArithmeticOperation.Subtract: result -= this.FixedOperand; break;
+                case eArithmeticOperation.Multiply: result *= this.FixedOperand; break;
+                case eArithmeticOperation.Divide:
+                    if (this.FixedOperand != 0.0)
+                    {
+                        result /= this.FixedOperand;
+                    }
+                    break;
+                default: break;
+            }
+
+            return System.Convert.ChangeType(result, targetType);
+        }
+    }
+}
