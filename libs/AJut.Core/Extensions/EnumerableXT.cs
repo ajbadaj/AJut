a68d5a6e662db75ff03a2eef3c42640ced26044d
AJ <ajbadarni@gmail.com>
2021-09-30 23:51:30 -0700

    [libs][core] formatting cleanup

--- libs/AJut.Core/Extensions/EnumerableXT.cs
+++ libs/AJut.Core/Extensions/EnumerableXT.cs
@@ -5,73 +5,73 @@
     using System.Collections.Generic;
     using System.Linq;
 
-    public delegate bool Matcher<T>(T first, T second);
+    public delegate bool Matcher<T> (T first, T second);
 
-	public static class EnumerableExtensions
-	{
-        public static bool IsEmpty<T>(this IEnumerable<T> This)
+    public static class EnumerableExtensions
+    {
+        public static bool IsEmpty<T> (this IEnumerable<T> This)
         {
             return !This.Any();
         }
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> This)
+        public static bool IsNullOrEmpty<T> (this IEnumerable<T> This)
         {
             return This == null || !This.Any();
         }
-		public static bool IsNotEmpty<T>(this IEnumerable<T> This)
-		{
-			return This.Any();
-		}
-		public static bool IsNotNullOrEmpty<T>(this IEnumerable<T> This)
-		{
-			return This != null && This.Any();
-		}
-		public static IEnumerable Cast(this IEnumerable This, Type type)
-		{
-			return This.InvokeTemplatedExtensionMethod(typeof(Enumerable), "Cast", type) as IEnumerable;
-		}
-		public static Array ToArray(this IEnumerable This, Type type)
-		{
-			return This.InvokeTemplatedExtensionMethod(typeof(Enumerable), "ToArray", type) as Array;
-		}
-		public static Array CastToArrayOfType(this IEnumerable This, Type type)
-		{
-			IEnumerable result = This.Cast(type);
+        public static bool IsNotEmpty<T> (this IEnumerable<T> This)
+        {
+            return This.Any();
+        }
+        public static bool IsNotNullOrEmpty<T> (this IEnumerable<T> This)
+        {
+            return This != null && This.Any();
+        }
+        public static IEnumerable Cast (this IEnumerable This, Type type)
+        {
+            return This.InvokeTemplatedExtensionMethod(typeof(Enumerable), "Cast", type) as IEnumerable;
+        }
+        public static Array ToArray (this IEnumerable This, Type type)
+        {
+            return This.InvokeTemplatedExtensionMethod(typeof(Enumerable), "ToArray", type) as Array;
+        }
+        public static Array CastToArrayOfType (this IEnumerable This, Type type)
+        {
+            IEnumerable result = This.Cast(type);
             if (result == null)
             {
                 return null;
             }
 
-			return result.ToArray(type);
-		}
+            return result.ToArray(type);
+        }
 
-		public static string ConcatToString(this IEnumerable This, string separator)
-		{
-            return String.Join(separator, This.OfType<object>().Select(_ => _.ToString()).ToArray());
-		}
-
-		public static string ConcatToString(this IEnumerable This, string separator, string prefix, string suffix)
-		{
-            return prefix + This.ConcatToString(separator) + suffix;
-		}
-
-		public static bool ContainsAny<T>(this IEnumerable<T> This, Comparison<T> comparer, IEnumerable<T> other)
-		{
-			foreach (T thisObj in This)
-			{
-				foreach (T otherObj in other)
-				{
-					if (comparer(thisObj, otherObj) == 0)
-						return true;
-				}
-			}
-
-			return false;
-		}
-        public static bool ContainsAnyThatPass<T>(this IEnumerable<T> This, Func<T, bool> matcher)
+        public static string ConcatToString (this IEnumerable This, string separator)
         {
-            foreach(T element in This)
+            return String.Join(separator, This.OfType<object>().Select(_ => _.ToString()).ToArray());
+        }
+
+        public static string ConcatToString (this IEnumerable This, string separator, string prefix, string suffix)
+        {
+            return prefix + This.ConcatToString(separator) + suffix;
+        }
+
+        public static bool ContainsAny<T> (this IEnumerable<T> This, Comparison<T> comparer, IEnumerable<T> other)
+        {
+            foreach (T thisObj in This)
             {
-                if(matcher(element))
+                foreach (T otherObj in other)
+                {
+                    if (comparer(thisObj, otherObj) == 0)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+        public static bool ContainsAnyThatPass<T> (this IEnumerable<T> This, Func<T, bool> matcher)
+        {
+            foreach (T element in This)
+            {
+                if (matcher(element))
                 {
                     return true;
                 }
@@ -80,21 +80,21 @@
             return false;
         }
 
-        public static bool ContainsAny<T>(this IEnumerable<T> This, IEnumerable<T> other)
-		{
-			foreach (object thisObj in This)
-			{
-				foreach (object otherObj in other)
-				{
-					if (thisObj == otherObj)
-						return true;
-				}
-			}
+        public static bool ContainsAny<T> (this IEnumerable<T> This, IEnumerable<T> other)
+        {
+            foreach (object thisObj in This)
+            {
+                foreach (object otherObj in other)
+                {
+                    if (thisObj == otherObj)
+                        return true;
+                }
+            }
 
-			return false;
-		}
+            return false;
+        }
 
-        public static void ForEach(this IEnumerable This, Action<object> toDo)
+        public static void ForEach (this IEnumerable This, Action<object> toDo)
         {
             foreach (object element in This)
             {
@@ -102,12 +102,32 @@
             }
         }
 
-        public static void ForEach<T>(this IEnumerable<T> This, Action<T> toDo)
+        public static void ForEach<T> (this IEnumerable<T> This, Action<T> toDo)
         {
-            foreach(T element in This)
+            foreach (T element in This)
             {
                 toDo?.Invoke(element);
             }
         }
-	}    
+
+        /// <summary>
+        /// To use when you are consuming an enumerable that you need IList interface for, but you
+        /// might be using something casted to IEnumerable. ToList is the fastest if you're converting
+        /// something - but if you don't need to, casting is fastest. This might be good if you need
+        /// array indexing, count, or just to do more than one thing in a situation where you don't
+        /// know if you *can* go over the list twice.
+        /// 
+        /// In short, this will, give you the fastest possible <see cref="IReadOnlyList{T}"/> version 
+        /// of the given enumerable that you can do what you like with.
+        /// </summary>
+        public static IReadOnlyList<T> ToReadOnlyListTryCastingFirst<T>(this IEnumerable<T> enumerable)
+        {
+            if (enumerable is IReadOnlyList<T> readOnlyList)
+            {
+                return readOnlyList;
+            }
+
+            return enumerable.ToList();
+        }
+    }
 }
