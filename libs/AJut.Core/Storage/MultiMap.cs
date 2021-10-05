80e005fe6ef1209aa44f6675ade78e65d38cbedf
AJ <ajbadarni@gmail.com>
2021-10-05 01:41:29 -0700

    [libs][core] MultiMap - finalization, cleanup, and determination (yes) if it should stay

--- libs/AJut.Core/Storage/MultiMap.cs
+++ libs/AJut.Core/Storage/MultiMap.cs
@@ -1,39 +1,68 @@
 ﻿namespace AJut.Storage
 {
-    using System;
     using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
 
-    // ================================================================================================================
-    // ===[ AJut note: Expect Major Rework ]===
-    //
-    // Just messing with this file to say... oof. This needs optimization. None of the hashing beneifts of Dictionary
-    //  just to make an easier multi element interface? Huge mistake, will have to rework :/
-
+    /// <summary>
+    /// A simple utility for simplifying usage of Dictionary where the value is a list of values.
+    /// </summary>
     public class MultiMap<TKey, TValue> : IEnumerable<KeyValuePair<TKey, List<TValue>>>
     {
         private readonly Dictionary<TKey, List<TValue>> m_store = new Dictionary<TKey, List<TValue>>();
 
+        /// <summary>
+        /// Get value list (or create it if it doesn't exist) for the given key
+        /// </summary>
         public List<TValue> this [TKey key] => this.GetValuesFor(key, true);
 
-        public virtual void AddFrom<T> (T valueSource, Func<T, TKey> keySelector, Func<T, IEnumerable<TValue>> valuesSelector)
-        {
-            this.GetValuesFor(keySelector(valueSource), true).AddRange(valuesSelector(valueSource));
-        }
-
+        /// <summary>
+        /// Add the key (and the list of values for that list if the key doesn't exist) and value to the key's list.
+        /// </summary>
         public virtual void Add (TKey key, TValue value)
         {
             this.GetValuesFor(key, true).Add(value);
         }
 
-        public virtual bool Remove (TKey key, TValue value)
+        /// <summary>
+        /// Remove value for a given key (and optionally remove the key if it's the last value)
+        /// </summary>
+        public virtual bool Remove (TKey key, TValue value, bool clearIfLast = true)
         {
-            return this.GetValuesFor(key, false)?.Remove(value) ?? false;
+            var values = this.GetValuesFor(key, false);
+            if (values?.Remove(value) ?? false)
+            {
+                // If it's the last value - nix the list
+                if (clearIfLast && values.Count == 0)
+                {
+                    m_store.Remove(key);
+                }
+
+                return true;
+            }
+
+            return false;
         }
 
+        /// <summary>
+        /// Remove all instances of the given value from all lists across all keys
+        /// </summary>
+        public void RemoveAllValues (TValue value)
+        {
+            foreach (var key in m_store.Keys)
+            {
+                this.Remove(key, value);
+            }
+        }
+
+        /// <summary>
+        /// Determine the total element count
+        /// </summary>
         public int GetTotalElementCount() => m_store.Values.Sum(v => v.Count);
 
+        /// <summary>
+        /// Get the count for a given key's list (zero if key has not been stored)
+        /// </summary>
         public virtual int GetCountFor (TKey key)
         {
             return this.GetValuesFor(key, false)?.Count ?? 0;
@@ -55,18 +84,15 @@
             return values;
         }
 
-        public IEnumerator<KeyValuePair<TKey, List<TValue>>> GetEnumerator () => m_store.GetEnumerator();
-        IEnumerator IEnumerable.GetEnumerator () => m_store.GetEnumerator();
+        /// <summary>
+        /// Enumerator for iterating
+        /// </summary>
+        IEnumerator<KeyValuePair<TKey, List<TValue>>> IEnumerable<KeyValuePair<TKey, List<TValue>>>.GetEnumerator () => m_store.GetEnumerator();
 
         /// <summary>
-        /// Removes value from all zones
+        /// Enumerator for iterating
         /// </summary>
-        public void RemoveAllValues (TValue value)
-        {
-            foreach (var key in m_store.Keys)
-            {
-                this.Remove(key, value);
-            }
-        }
+        IEnumerator IEnumerable.GetEnumerator () => m_store.GetEnumerator();
+
     }
 }
