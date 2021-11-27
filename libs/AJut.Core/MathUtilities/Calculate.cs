4befe634c561f9befbf1a0cebc57d24bbf45d533
AJ <ajbadarni@gmail.com>
2021-11-27 14:04:15 -0800

    [libs][core] Updates to math utils: Calculate - Adding new min/max utilities, enforcing use of dynamic, commenting

--- libs/AJut.Core/MathUtilities/Calculate.cs
+++ libs/AJut.Core/MathUtilities/Calculate.cs
@@ -4,50 +4,60 @@
     using System.Collections.Generic;
     using System.Linq;
 
+    /// <summary>
+    /// Math oriented calculation helpers
+    /// </summary>
     public static class Calculate
     {
-        public static double Mean (IEnumerable<double> numbers)
+        /// <summary>
+        /// Calculate the mean average of the number set
+        /// </summary>
+        public static dynamic Mean (IEnumerable<dynamic> numbers)
         {
-            return numbers.Sum() / numbers.Count();
-        }
-        public static double Mean (params double[] numbers)
-        {
-            return numbers.Sum() / numbers.Length;
-        }
-        public static T LeastCommonMultiple<T> (IEnumerable<T> numbers)
-        {
-            return numbers.Aggregate((T a, T b) => (T)LeastCommonMultipleFixed(a, b));
+            return Sum(numbers) / numbers.Count();
         }
 
+        /// <summary>
+        /// Calculate the least common multiple of the number set
+        /// </summary>
+        public static dynamic LeastCommonMultiple<dynamic> (IEnumerable<dynamic> numbers)
+        {
+            return numbers.Aggregate((dynamic a, dynamic b) => LeastCommonMultipleFixed(a, b));
+        }
+
+        /// <summary>
+        /// Calculate the least common multiple of the number set
+        /// </summary>
         public static dynamic LeastCommonMultiple (params dynamic[] numbers)
         {
             return numbers.Aggregate(LeastCommonMultipleFixed);
         }
 
-        private static dynamic LeastCommonMultipleFixed (dynamic a, dynamic b)
+        /// <summary>
+        /// Calculate the greatest common denominator of the number set
+        /// </summary>
+        public static dynamic GreatestCommonDenominator<dynamic> (IEnumerable<dynamic> numbers)
         {
-            return Math.Abs(a * b) / GreatestCommonDenominator(a, b);
+            return numbers.Aggregate((dynamic a, dynamic b) => GreatestCommonDenominatorFixed(a, b));
         }
 
-        public static T GreatestCommonDenominator<T> (IEnumerable<T> numbers)
-        {
-            return numbers.Aggregate((T a, T b) => (T)GreatestCommonDenominatorFixed(a, b));
-        }
-
+        /// <summary>
+        /// Calculate the greatest common denominator of the number set
+        /// </summary>
         public static dynamic GreatestCommonDenominator (params dynamic[] numbers)
         {
             return numbers.Aggregate(GreatestCommonDenominatorFixed);
         }
 
-        public static void MinAndMaxValuesIn<T> (IEnumerable<T> numbers, out T min, out T max)
+        /// <summary>
+        /// Allows you to iterate over a sequence once and extract min and max values at the same time.
+        /// </summary>
+        /// <param name="numbers">The numbers to evaluate</param>
+        /// <param name="min">The found min value</param>
+        /// <param name="max">The found max value</param>
+        public static void MinAndMaxValuesIn (IEnumerable<dynamic> numbers, out dynamic min, out dynamic max)
         {
-            MinAndMaxValuesIn(numbers.ToArray(), out min, out max);
-        }
-
-        public static void MinAndMaxValuesIn (dynamic[] numbers, out dynamic min, out dynamic max)
-        {
-            min = numbers[0];
-            max = numbers[0];
+            min = max = numbers.First();
             foreach (dynamic value in numbers.Skip(1))
             {
                 if (value < min)
@@ -59,6 +69,27 @@
                     max = value;
                 }
             }
+        }
+
+        /// <summary>
+        /// Similar to the <see cref="Enumerable.Sum"/> extension functions, except accepting of dynamic
+        /// </summary>
+        public static dynamic Sum (IEnumerable<dynamic> numbers)
+        {
+            dynamic sum = 0;
+            foreach (dynamic value in numbers)
+            {
+                sum += value;
+            }
+
+            return sum;
+        }
+
+        // ==========================[ Private Utility Functions ]===========================================
+
+        private static dynamic LeastCommonMultipleFixed (dynamic a, dynamic b)
+        {
+            return Math.Abs(a * b) / GreatestCommonDenominator(a, b);
         }
 
         private static dynamic GreatestCommonDenominatorFixed (dynamic a, dynamic b)
