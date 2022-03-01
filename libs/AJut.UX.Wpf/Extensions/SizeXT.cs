975b12c953051d14a7e12178695f087f8da56959
AJ <ajbadarni@gmail.com>
2022-02-28 22:42:51 -0800

    [libs][ux] Adding rect extension for zero area

--- libs/AJut.UX.Wpf/Extensions/SizeXT.cs
+++ libs/AJut.UX.Wpf/Extensions/SizeXT.cs
@@ -4,7 +4,7 @@
 
     public static class SizeXT
     {
-        public static bool HasZeroArea(this Size size)
+        public static bool HasZeroArea (this Size size)
         {
             return size.Width == 0.0 || size.Height == 0.0;
         }
