1947fb8415e44537afbdadd2e7b309a34dcf6c28
AJ <ajbadarni@gmail.com>
2022-08-02 22:21:01 -0400

    Start for color edit ingress notifying user edit complete

--- libs/AJut.UX.Wpf/Controls/ColorEditIngressControl.cs
+++ libs/AJut.UX.Wpf/Controls/ColorEditIngressControl.cs
@@ -1,23 +1,46 @@
 ﻿namespace AJut.UX.Controls
 {
+    using System;
     using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Input;
     using System.Windows.Media;
     using AJut.UX.AttachedProperties;
+    using AJut.UX.Event;
     using DPUtils = DPUtils<ColorEditIngressControl>;
 
     public class ColorEditIngressControl : Control
     {
+        private Color? m_editCache;
         static ColorEditIngressControl()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorEditIngressControl), new FrameworkPropertyMetadata(typeof(ColorEditIngressControl)));
         }
-
+        
         public ColorEditIngressControl()
         {
+            this.Focusable = true;
             ClickableXTA.SetIsTrackingClick(this, true);
             ClickableXTA.AddClickHandler(this, (e, a) => this.ShowEditDisplay = !this.ShowEditDisplay);
         }
+
+        protected override void OnKeyUp (KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                this.ShowEditDisplay = true;
+            }
+            else
+            {
+                base.OnKeyUp(e);
+            }
+        }
+
+        /// <summary>
+        /// An event that signifies a user edit has completed - this is slightly different than changes to the <see cref="EditColor"/> in that
+        /// it also signifies an edit initiation, change, and completion have both occurred - not just a change.
+        /// </summary>
+        public event EventHandler<UserEditEventArgs<Color>> UserEditComplete;
 
         public static readonly DependencyProperty EditColorProperty = DPUtils.Register(_ => _.EditColor);
         public Color EditColor
@@ -68,11 +91,32 @@
             set => this.SetValue(PopupWidthProperty, value);
         }
 
-        public static readonly DependencyProperty ShowEditDisplayProperty = DPUtils.Register(_ => _.ShowEditDisplay, false);
+        public static readonly DependencyProperty ShowEditDisplayProperty = DPUtils.Register(_ => _.ShowEditDisplay, false, (d,e)=>d.OnShowEditDisplayChanged(e));
         public bool ShowEditDisplay
         {
             get => (bool)this.GetValue(ShowEditDisplayProperty);
             set => this.SetValue(ShowEditDisplayProperty, value);
         }
+
+        private void OnShowEditDisplayChanged (DependencyPropertyChangedEventArgs<bool> e)
+        {
+            if (e.NewValue)
+            {
+                m_editCache = this.EditColor;
+            }
+            else
+            {
+                if (!(m_editCache?.Equals(this.EditColor) ?? false))
+                {
+                    this.UserEditComplete?.Invoke(this, new UserEditEventArgs<Color>(m_editCache.Value, this.EditColor));
+
+                }
+
+                m_editCache = null;
+
+            }
+
+        }
+
     }
 }
---
+++ libs/AJut.UX.Wpf/Event/UserEditEventArgs.cs
@@ -0,0 +1,16 @@
+﻿namespace AJut.UX.Event
+{
+    using System;
+
+    public class UserEditEventArgs<T> : EventArgs
+    {
+        public UserEditEventArgs (T previousData, T newData)
+        {
+            this.PreviousData = previousData;
+            this.NewData = newData;
+        }
+
+        public T PreviousData { get; }
+        public T NewData { get; }
+    }
+}
