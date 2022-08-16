4934ceb8fb0f1c9e51842fe452611dfb78a73662
AJ <ajbadarni@gmail.com>
2022-08-16 00:07:13 -0400

    Sandbox - Small update to code display to use line numbers, pretty silly and stupid really but a slight aid

--- sandbox/UI/Controls/BasicCodeDisplay.xaml.cs
+++ sandbox/UI/Controls/BasicCodeDisplay.xaml.cs
@@ -13,11 +13,47 @@
             this.InitializeComponent();
         }
 
-        public static readonly DependencyProperty CodeTextProperty = DPUtils.Register(_ => _.CodeText);
+        public static readonly DependencyProperty CodeTextProperty = DPUtils.Register(_ => _.CodeText, (d,e)=>d.ResetDisplayText());
         public string CodeText
         {
             get => (string)this.GetValue(CodeTextProperty);
             set => this.SetValue(CodeTextProperty, value);
+        }
+
+
+        public static readonly DependencyProperty ShowLineNumbersProperty = DPUtils.Register(_ => _.ShowLineNumbers, true, (d, e) => d.ResetDisplayText());
+        public bool ShowLineNumbers
+        {
+            get => (bool)this.GetValue(ShowLineNumbersProperty);
+            set => this.SetValue(ShowLineNumbersProperty, value);
+        }
+
+
+        private static readonly DependencyPropertyKey FinalTransformedTextPropertyKey = DPUtils.RegisterReadOnly(_ => _.FinalTransformedText);
+        public static readonly DependencyProperty FinalTransformedTextProperty = FinalTransformedTextPropertyKey.DependencyProperty;
+        public string FinalTransformedText
+        {
+            get => (string)this.GetValue(FinalTransformedTextProperty);
+            protected set => this.SetValue(FinalTransformedTextPropertyKey, value);
+        }
+
+
+        private void ResetDisplayText ()
+        {
+            if (this.ShowLineNumbers)
+            {
+                string[] allCodeText = this.CodeText.Replace("\r\n", "\n").Split("\n");
+                for (int index = 0; index < allCodeText.Length; ++index)
+                {
+                    allCodeText[index] = $"{index:00}: {allCodeText[index]}";
+                }
+
+                this.FinalTransformedText = String.Join('\n', allCodeText);
+            }
+            else
+            {
+                this.FinalTransformedText = this.CodeText;
+            }
         }
 
         private void CopyCode_OnClick (object sender, RoutedEventArgs e)
