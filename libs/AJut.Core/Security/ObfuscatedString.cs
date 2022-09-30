4b827f7f3be261e9259aac31442e775d3afd7c6a
AJ <33132125+ajbadaj@users.noreply.github.com>
2022-09-30 13:43:23 -0400

    Adding obfuscated string, needed to support Any Cpu (which defaults to x86) and x64 architectures - both generate different obfuscated strings (#23)

---
+++ libs/AJut.Core/Security/ObfuscatedString.cs
@@ -0,0 +1,24 @@
+﻿namespace AJut.Security
+{
+    using System;
+
+    /// <summary>
+    /// Storage for encrypted obfuscated strings, stores both the basic string, and the x64 compiled version and provides whichever is active.
+    /// </summary>
+    public class ObfuscatedString
+    {
+        private readonly string m_str;
+        private readonly string m_x64Str;
+
+        public ObfuscatedString (string str, string x64str)
+        {
+            m_str = str;
+            m_x64Str = x64str;
+        }
+
+        /// <summary>
+        /// The architecture target determined, encrypted, string to utilize
+        /// </summary>
+        public string Active => Environment.Is64BitProcess ? m_x64Str : m_str;
+    }
+}
