9a548fc76635c024852031944e92fadebc92af02
AJ <ajbadarni@gmail.com>
2022-11-04 03:13:16 -0400

    New convereters: * Nested corner radius converter when you want to bind to a corner radius but maybe have some nested ones but the inner one needs to adjust it's corner radius * ParameterizedBorderGapMaskConverter - this is pretty much exclusively for the new GroupBox styles

---
+++ libs/AJut.UX.Wpf/BindingUtils/Converters/ParameterizedBorderGapMaskConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace AJut.UX.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Markup;
+
+    public class ParameterizedBorderGapMaskConverter : MarkupExtension, IMultiValueConverter
+    {
+        private readonly BorderGapMaskConverter m_borderGapMaskConverter = new BorderGapMaskConverter();
+        public double GapStart { get; set; }
+
+        public override object ProvideValue (IServiceProvider serviceProvider) => this;
+
+        public object Convert (object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return m_borderGapMaskConverter.Convert(values, targetType, this.GapStart, culture);
+        }
+
+        public object[] ConvertBack (object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
