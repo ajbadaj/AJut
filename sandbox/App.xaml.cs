665105d6c8a86f8e872fa0b32c918fef3456310e
AJ <ajbadarni@gmail.com>
2022-11-11 14:53:15 -0500

    [sandbox] Moving towards the navigator root being in App

--- sandbox/App.xaml.cs
+++ sandbox/App.xaml.cs
@@ -1,17 +1,26 @@
 ﻿namespace TheAJutShowRoom
 {
     using System;
+    using System.ComponentModel;
     using System.Diagnostics;
+    using System.IO;
     using System.Windows;
     using AJut;
+    using AJut.Text.AJson;
     using AJut.TypeManagement;
     using AJut.UX;
+    using AJut.UX.Controls;
     using AJut.UX.Theming;
 
-    public partial class App : Application
+    public partial class App : Application, INotifyPropertyChanged
     {
+        private const string kConfigFileName = ".ajut-showroom-config";
         public static Random kRNG = new Random(DateTime.Now.Millisecond);
+        private static bool g_restrictThemeChangeNotification = true;
+        private readonly ResourceDictionary?[] m_themeSaves = new ResourceDictionary?[2];
+        private bool m_useThemes = true;
 
+        // ==================[ Construction ]========================
         static App ()
         {
             var assembly = typeof(Logger).Assembly;
@@ -23,6 +32,8 @@
             versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
             AJut_Ux_Wpf_Version = versionInfo.ProductVersion?.ToString() ?? "unknown version";
             Logger.LogInfo($"Using AJut.UX.Wpf version #{AJut_Ux_Wpf_Version}");
+
+            App.Navigator = new StackNavFlowController();
         }
         public App ()
         {
@@ -39,12 +50,52 @@
         protected override void OnStartup (StartupEventArgs e)
         {
             ThemeTracker = new AppThemeManager();
-            ThemeTracker.ThemeConfiguration = eAppThemeConfiguration.UseSameAsOS;
+
+            AJutShowroomAppConfig config = GetConfig();
+            ThemeTracker.ThemeConfiguration = config.Theme;
             base.OnStartup(e);
+
+            this.UseThemes = config.UseThemes;
+            ThemeTracker.PropertyChanged += _OnThemeTrackerChanged;
+
+            g_restrictThemeChangeNotification = false;
+
+            App.Navigator.GenerateAndPushDisplay<UI.Pages.LandingPage>();
+
+            void _OnThemeTrackerChanged (object? sender, PropertyChangedEventArgs e)
+            {
+                SaveConfig();
+            }
         }
+
+        // ==================[ Events ]========================
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // ==================[ Properties ]========================
+        public static string AJut_Core_Version { get; }
+        public static string AJut_Ux_Wpf_Version { get; }
+        public static StackNavFlowController Navigator { get; }
 
         public static AppThemeManager? ThemeTracker { get; private set; }
 
+        public bool UseThemes
+        {
+            get => m_useThemes;
+            set 
+            {
+                if (m_useThemes == value)
+                {
+                    return;
+                }
+
+                m_useThemes = value;
+                this.SetUseThemes(value);
+                App.SaveConfig();
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(UseThemes)));
+            }
+        }
+
+        // ==================[ Private Utilities ]========================
         private static bool UnhandledExceptionProcessor (Exception e)
         {
             Logger.LogError(e);
@@ -52,19 +103,7 @@
             return result == MessageBoxResult.Yes;
         }
 
-        public static string AppDataPath(string pathEnd)
-        {
-            return System.IO.Path.Combine(ApplicationUtilities.AppDataRoot, pathEnd);
-        }
-
-        private readonly ResourceDictionary?[] m_themeSaves = new ResourceDictionary?[2];
-
-        public static void SetUseThemes (bool useThemes)
-        {
-            ((App)App.Current).DoSetUseThemes(useThemes);
-        }
-
-        private void DoSetUseThemes (bool useThemes)
+        private async void SetUseThemes (bool useThemes)
         {
             if (useThemes)
             {
@@ -81,9 +120,75 @@
                 this.Resources.MergedDictionaries.Remove(m_themeSaves[0]);
                 this.Resources.MergedDictionaries.Remove(m_themeSaves[1]);
             }
+
+            SaveConfig();
+
+            if (g_restrictThemeChangeNotification)
+            {
+                string msg = m_useThemes ? "on" : "off";
+                await App.Navigator.StackTopDisplayAdapter.ShowPopover(MessageBoxPopover.Generate($"Themes are now {msg}, this may require restart to take proper effect"));
+            }
         }
 
-        public static string AJut_Core_Version { get; }
-        public static string AJut_Ux_Wpf_Version { get; }
+
+        private static void SaveConfig ()
+        {
+            string configFilePath = ApplicationUtilities.BuildAppDataProjectPath(kConfigFileName);
+
+            Json json = JsonHelper.BuildJsonForObject(new AJutShowroomAppConfig
+            {
+                Theme = ThemeTracker?.ThemeConfiguration ?? eAppThemeConfiguration.UseSameAsOS,
+                UseThemes = ((App)App.Current).UseThemes,
+            });
+
+            if (!json)
+            {
+                Logger.LogError($"Failed to build json for showroom config - errors were: {json.GetErrorReport()}");
+                return;
+            }
+
+            File.Delete(configFilePath);
+            File.WriteAllText(configFilePath, json.ToString());
+        }
+
+        private static AJutShowroomAppConfig GetConfig ()
+        {
+            var defaultConfig = new AJutShowroomAppConfig
+            {
+                Theme = ThemeTracker?.ThemeConfiguration ?? eAppThemeConfiguration.UseSameAsOS,
+                UseThemes = true,
+            };
+
+            string configFilePath = ApplicationUtilities.BuildAppDataProjectPath(kConfigFileName);
+            if (File.Exists(configFilePath))
+            {
+                Json configJson = JsonHelper.ParseFile(configFilePath);
+                if (configJson)
+                {
+                    return _GetConfig(JsonHelper.BuildObjectForJson<AJutShowroomAppConfig>(configJson));
+                }
+
+                Logger.LogError($"Failed to build json for showroom config - errors were: {configJson.GetErrorReport()}");
+            }
+
+            return _GetConfig(null);
+
+            AJutShowroomAppConfig _GetConfig (AJutShowroomAppConfig _built)
+            {
+                if (_built == null)
+                {
+                    Logger.LogInfo("Due to invalid or missing config, building new default showroom config");
+                    return defaultConfig;
+                }
+
+                return _built;
+            }
+        }
+
+        private class AJutShowroomAppConfig
+        {
+            public eAppThemeConfiguration Theme { get; set; }
+            public bool UseThemes { get; set; }
+        }
     }
 }
--- sandbox/MainWindow.xaml.cs
+++ sandbox/MainWindow.xaml.cs
@@ -1,25 +1,12 @@
 ﻿namespace TheAJutShowRoom
 {
     using System.Windows;
-    using AJut.UX;
-
-    using DPUtils = AJut.UX.DPUtils<MainWindow>;
-
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             this.InitializeComponent();
-            this.Navigator.GenerateAndPushDisplay<UI.Pages.LandingPage>();
         }
-
-        public static readonly DependencyProperty UseThemesProperty = DPUtils.Register(_ => _.UseThemes, true, (d, e) => App.SetUseThemes(e.NewValue));
-        public bool UseThemes
-        {
-            get => (bool)this.GetValue(UseThemesProperty);
-            set => this.SetValue(UseThemesProperty, value);
-        }
-
-        public StackNavFlowController Navigator { get; } = new StackNavFlowController();
     }
 }
