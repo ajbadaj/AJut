83fa206a7f412e26b2110a378e16f3c75b46a050
AJ <33132125+ajbadaj@users.noreply.github.com>
2023-02-07 02:25:17 -0500

    Stacknav ux/themed improvements (#50)
    * Stack nav visual improvements
    * New properties elevated so they can be configured, and most importantly put back to the way they were before if you want.
    * Defaults of new properties set is better than how the old way worked
    * MessageBoxPopover working with the new way to improve it's look

--- libs/AJut.UX.Wpf/Controls/StackNavDisplayElements/StackNavBusyWaitOverlay.cs
+++ libs/AJut.UX.Wpf/Controls/StackNavDisplayElements/StackNavBusyWaitOverlay.cs
@@ -34,5 +34,34 @@
             get => (DataTemplateSelector)this.GetValue(MessageContentTemplateSelectorProperty);
             set => this.SetValue(MessageContentTemplateSelectorProperty, value);
         }
+
+        public static readonly DependencyProperty OverlayColumnMarginWidthProperty = DPUtils.Register(_ => _.OverlayColumnMarginWidth);
+        public GridLength OverlayColumnMarginWidth
+        {
+            get => (GridLength)this.GetValue(OverlayColumnMarginWidthProperty);
+            set => this.SetValue(OverlayColumnMarginWidthProperty, value);
+        }
+
+        public static readonly DependencyProperty OverlayRowMarginHeightProperty = DPUtils.Register(_ => _.OverlayRowMarginHeight);
+        public GridLength OverlayRowMarginHeight
+        {
+            get => (GridLength)this.GetValue(OverlayRowMarginHeightProperty);
+            set => this.SetValue(OverlayRowMarginHeightProperty, value);
+        }
+
+        public static readonly DependencyProperty OverlayWidthProperty = DPUtils.Register(_ => _.OverlayWidth, GridLength.Auto);
+        public GridLength OverlayWidth
+        {
+            get => (GridLength)this.GetValue(OverlayWidthProperty);
+            set => this.SetValue(OverlayWidthProperty, value);
+        }
+
+        public static readonly DependencyProperty OverlayHeightProperty = DPUtils.Register(_ => _.OverlayHeight, GridLength.Auto);
+        public GridLength OverlayHeight
+        {
+            get => (GridLength)this.GetValue(OverlayHeightProperty);
+            set => this.SetValue(OverlayHeightProperty, value);
+        }
+
     }
 }
--- libs/AJut.UX.Wpf/Controls/StackNavDisplayElements/StackNavPopoverOverlay.cs
+++ libs/AJut.UX.Wpf/Controls/StackNavDisplayElements/StackNavPopoverOverlay.cs
@@ -82,6 +82,34 @@
             set => this.SetValue(HighlightBorderPaddingProperty, value);
         }
 
+        public static readonly DependencyProperty OverlayColumnMarginWidthProperty = DPUtils.Register(_ => _.OverlayColumnMarginWidth);
+        public GridLength OverlayColumnMarginWidth
+        {
+            get => (GridLength)this.GetValue(OverlayColumnMarginWidthProperty);
+            set => this.SetValue(OverlayColumnMarginWidthProperty, value);
+        }
+
+        public static readonly DependencyProperty OverlayRowMarginHeightProperty = DPUtils.Register(_ => _.OverlayRowMarginHeight);
+        public GridLength OverlayRowMarginHeight
+        {
+            get => (GridLength)this.GetValue(OverlayRowMarginHeightProperty);
+            set => this.SetValue(OverlayRowMarginHeightProperty, value);
+        }
+
+        public static readonly DependencyProperty OverlayWidthProperty = DPUtils.Register(_ => _.OverlayWidth, GridLength.Auto);
+        public GridLength OverlayWidth
+        {
+            get => (GridLength)this.GetValue(OverlayWidthProperty);
+            set => this.SetValue(OverlayWidthProperty, value);
+        }
+
+        public static readonly DependencyProperty OverlayHeightProperty = DPUtils.Register(_ => _.OverlayHeight, GridLength.Auto);
+        public GridLength OverlayHeight
+        {
+            get => (GridLength)this.GetValue(OverlayHeightProperty);
+            set => this.SetValue(OverlayHeightProperty, value);
+        }
+
         private void OuterContents_OnClick (object sender, MouseButtonEventArgs e)
         {
             this.DisplayContent.CancelViaClickAway();
