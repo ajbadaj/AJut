4bfe7b7f03faf2f8f4e8328e8b434725d5af2a0d
ajbadaj <ajbadarni@gmail.com>
2026-03-02 13:51:34 -0500

    Updates to color edit + new converters so that they could work together better

--- libs/AJut.UX.WinUI3/Converters/ColorStringToColorConverter.cs
+++ libs/AJut.UX.WinUI3/Converters/ColorStringToColorConverter.cs
@@ -1,9 +1,14 @@
 namespace AJut.UX.Converters
 {
+    using AJut.UX.Helpers;
     using Windows.UI;
 
     public class ColorStringToColorConverter : SimpleValueConverter<string, Color>
     {
         protected override Color Convert (string value) => CoerceUtils.CoerceColorFrom(value);
+        protected override string ConvertBack(Color color)
+        {
+            return AJutColorHelper.GetSmallestHexString(color.A, color.R, color.G, color.B);
+        }
     }
 }
--- libs/AJut.UX.Wpf/BindingUtils/Converters/ColorStringToColorConverter.cs
+++ libs/AJut.UX.Wpf/BindingUtils/Converters/ColorStringToColorConverter.cs
@@ -1,9 +1,14 @@
 namespace AJut.UX.Converters
 {
+    using AJut.UX.Helpers;
     using System.Windows.Media;
 
     public class ColorStringToColorConverter : SimpleValueConverter<string, Color>
     {
         protected override Color Convert (string value) => CoerceUtils.CoerceColorFrom(value);
+        protected override string ConvertBack(Color color)
+        {
+            return AJutColorHelper.GetSmallestHexString(color.A, color.R, color.G, color.B);
+        }
     }
 }
